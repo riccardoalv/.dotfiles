c05f811dcde482ee92c51369e3d937903a8c5850
Ricardo Alves da Silva <ricardcpu@gmail.com>
2022-10-06 16:14:15 -0400

    Change register for deletes

--- nvim/.config/nvim/lua/ricardo/keymaps.lua
+++ nvim/.config/nvim/lua/ricardo/keymaps.lua
@@ -31,9 +31,10 @@ keymap("n", "<A-h>", ":noh<cr>", opts)
 
 -- copy
 keymap("n", "Y", "y$", opts)
-keymap("n", "d", '"_d', opts)
-keymap("n", "c", '"_c', opts)
-keymap("n", "x", '"_x', opts)
+keymap("n", "d", '"cd', opts)
+keymap("n", "c", '"cc', opts)
+keymap("n", "x", '"cx', opts)
+keymap("n", "P", '"cp', opts)
 
 -- text-objectis quotes
 keymap("o", "ix", [[i']], opts)
