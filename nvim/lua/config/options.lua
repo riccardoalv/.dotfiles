aba36ef60bf744e74f679ade50177728898d850d
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-04-10 19:41:23 -0400

    hide tabline

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -80,7 +80,7 @@ o.visualbell = false
 
 -- Specify the behavior when switching between buffers
 o.switchbuf = "useopen,usetab,newtab"
-o.stal = 2
+o.stal = 0
 
 -- Always show the status line
 o.laststatus = 2
