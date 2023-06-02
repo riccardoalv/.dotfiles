b97616ed48a0df7e72a990cc36c22f7d893f0c70
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-06-02 12:10:24 -0400

    remove noice.nvim

--- nvim/lua/config/plugins/ui.lua
+++ nvim/lua/config/plugins/ui.lua
@@ -95,27 +95,6 @@ return {
 		end,
 	},
 	{
-		"folke/noice.nvim",
-        event = "VeryLazy",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-		},
-		opts = {
-			lsp = {
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-				},
-			},
-			presets = {
-				bottom_search = true,
-				command_palette = true,
-				long_message_to_split = true,
-			},
-		},
-	},
-
-	{
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
 		opts = function()
