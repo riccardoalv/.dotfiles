d499f33c8150df444e9dc413a7cbb45101eb652d
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-08-06 11:24:20 -0400

    update cpp keymaps

--- nvim/ftplugin/cpp.lua
+++ nvim/ftplugin/cpp.lua
@@ -1,1 +1,7 @@
-vim.o.makeprg = "g++ -g -O2 -Wall -std=gnu++17 '%' && ./a.out < '%:h/input.txt'"
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+vim.o.makeprg = "g++ -g -O2 -Wall -std=gnu++17 '%'"
+
+keymap("n", "<f11>", "<cmd>w<cr><cmd>make<cr>", opts)
+keymap("n", "<f12>", "<cmd>! ./a.out < '%:h/input.txt'<cr>", opts)
