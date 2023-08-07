be3c4ea2d3aa22f03e16e61c7fc4e5127c5107b7
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-08-07 09:23:21 -0400

    add minimal config for cpp

--- nvim/ftplugin/cpp.lua
+++ nvim/ftplugin/cpp.lua
@@ -1,7 +1,21 @@
+local o = vim.o
+
+o.expandtab = true
+o.smarttab = true
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+o.ai = true
+o.si = true
+o.wrap = false
+o.syntax = true
+
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-vim.o.makeprg = "g++ -g -O2 -Wall -std=gnu++17 '%'"
+vim.o.makeprg = "g++ -x c++ -g -O2 -Wall -std=gnu++20 '%'"
+
+keymap("i", "{<cr>", "{<cr><cr>}<up><tab>", opts)
 
 keymap("n", "<f11>", "<cmd>w<cr><cmd>make<cr>", opts)
 keymap("n", "<f12>", "<cmd>! ./a.out < '%:h/input.txt'<cr>", opts)
