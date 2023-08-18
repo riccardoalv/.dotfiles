857a5b92566d109c4fc9455cdeb72eb1184a1c81
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-08-18 18:28:26 -0400

    add autopep8

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -63,6 +63,7 @@ return {
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.autopep8,
         null_ls.builtins.diagnostics.pylint,
 			},
 		})
