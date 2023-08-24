fb6eb59db29b22204869d7941275074c35f78062
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-08-24 14:57:07 -0400

    add stylelint in null-ls

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -61,10 +61,11 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.nixfmt,
 				null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.autopep8,
         null_ls.builtins.diagnostics.pylint,
+        null_ls.builtins.diagnostics.stylelint,
+        null_ls.builtins.code_actions.gitsigns,
 			},
 		})
 
