536e71a358ba2dd5e3012d053074188cc41776d4
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-11-09 10:20:02 -0400

    disable powermanagement

--- system/laptop-configuration.nix
+++ system/laptop-configuration.nix
@@ -14,8 +14,8 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
