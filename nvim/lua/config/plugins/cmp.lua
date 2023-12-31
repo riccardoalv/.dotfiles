9d3b496f6c420398fdd3898b10789a7f6e80b896
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-12-31 16:16:15 -0400

    update nvim-cmp

--- nvim/lua/config/plugins/cmp.lua
+++ nvim/lua/config/plugins/cmp.lua
@@ -1,69 +1,58 @@
 return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+    },
 
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-		},
+    config = function()
+      local cmp = require("cmp")
 
-		config = function()
-			vim.cmd(
-				[[imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>']]
-			)
-			vim.cmd([[inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>]])
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-			vim.cmd([[snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>]])
-			vim.cmd([[snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>]])
+      vim.cmd(
+        [[imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>']]
+      )
+      vim.cmd([[inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>]])
+      vim.cmd([[snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>]])
+      vim.cmd([[snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>]])
 
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
-	},
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
 
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-			"saadparwaiz1/cmp_luasnip",
-		},
-
-		config = function()
-			local cmp = require("cmp")
-			local defaults = require("cmp.config.default")()
-
-			vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-
-			cmp.setup({
-				sorting = defaults.sorting,
-				experimental = {
-					ghost_text = {
-						hl_group = "CmpGhostText",
-					},
-				},
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				completion = {
-					completeopt = "menu,menuone,noinsert",
-				},
-				mapping = {
-					["<C-p>"] = cmp.mapping.select_prev_item(),
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-					["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-				},
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "buffer" },
-					{ name = "path" },
-					{ name = "luasnip" },
-				},
-			})
-		end,
-	},
+      cmp.setup({
+        experimental = {
+          ghost_text = {
+            hl_group = "CmpGhostText",
+          },
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
+        mapping = {
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "path" },
+          { name = "buffer" },
+          { name = "luasnip" },
+        },
+      })
+    end,
+  },
 }
--- nvim/lua/config/plugins/telescope.lua
+++ nvim/lua/config/plugins/telescope.lua
@@ -1,53 +1,51 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"debugloop/telescope-undo.nvim",
-	},
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "debugloop/telescope-undo.nvim",
+  },
 
-	config = function()
-		local keymap = vim.api.nvim_set_keymap
-		local opts = { noremap = true, silent = true }
+  config = function()
+    local keymap = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
 
-		telescope = require("telescope")
+    telescope = require("telescope")
 
-		-- Telescope.nvim
-		local actions = require("telescope.actions")
-		telescope.setup({
-			defaults = {
-				find_command = { "rg", "--ignore", "--hidden", "--files", git_dir },
-				-- winblend = 20,
-				mappings = {
-					n = {
-						["<C-q>"] = actions.send_to_qflist,
-						["d"] = actions.delete_buffer,
-					},
-					i = {
-						["<C-q>"] = actions.send_to_qflist,
-					},
-				},
-			},
-			extensions = {
-				undo = {
-					side_by_side = true,
-					layout_strategy = "vertical",
-				},
-			},
-		})
+    -- Telescope.nvim
+    local actions = require("telescope.actions")
+    telescope.setup({
+      defaults = {
+        mappings = {
+          n = {
+            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+            ["d"] = actions.delete_buffer,
+          },
+          i = {
+            ["<C-q>"] = actions.send_to_qflist,
+          },
+        },
+      },
+      extensions = {
+        undo = {
+          side_by_side = true,
+          layout_strategy = "vertical",
+        },
+      },
+    })
 
-		telescope.load_extension("undo")
-		telescope.load_extension("notify")
-		telescope.load_extension("git_worktree")
+    telescope.load_extension("undo")
+    telescope.load_extension("notify")
+    telescope.load_extension("git_worktree")
 
-		vim.cmd([[au FileType TelescopePrompt nmap <buffer> v <c-v>]])
-		vim.cmd([[au FileType TelescopePrompt nmap <buffer> x <c-x>]])
+    vim.cmd([[au FileType TelescopePrompt nmap <buffer> v <c-v>]])
+    vim.cmd([[au FileType TelescopePrompt nmap <buffer> x <c-x>]])
 
-		keymap("n", "<leader>f", '<cmd>lua require("telescope.builtin").git_files()<cr>', opts)
-		keymap("n", "<c-p>", '<cmd>lua require("telescope.builtin").find_files()<cr>', opts)
-		keymap("n", "<leader>s", '<cmd>lua require("telescope.builtin").git_status()<cr>', opts)
-		keymap("n", "<A-b>", '<cmd>lua require("telescope.builtin").git_branches()<cr>', opts)
-		keymap("n", "<A-q>", '<cmd>lua require("telescope.builtin").quickfix()<cr>', opts)
-		keymap("n", "<space>b", '<cmd>lua require("telescope.builtin").buffers()<cr>', opts)
-		keymap("n", "<leader>g", '<cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
-		keymap("n", "<A-t>", '<cmd>lua require("telescope.builtin").treesitter()<cr>', opts)
-	end,
+    keymap("n", "<leader>f", '<cmd>lua require("telescope.builtin").git_files()<cr>', opts)
+    keymap("n", "<c-p>", '<cmd>lua require("telescope.builtin").find_files()<cr>', opts)
+    keymap("n", "<leader>s", '<cmd>lua require("telescope.builtin").git_status()<cr>', opts)
+    keymap("n", "<A-b>", '<cmd>lua require("telescope.builtin").git_branches()<cr>', opts)
+    keymap("n", "<A-q>", '<cmd>lua require("telescope.builtin").quickfix()<cr>', opts)
+    keymap("n", "<space>b", '<cmd>lua require("telescope.builtin").buffers()<cr>', opts)
+    keymap("n", "<leader>g", '<cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
+    keymap("n", "<A-t>", '<cmd>lua require("telescope.builtin").treesitter()<cr>', opts)
+  end,
 }
