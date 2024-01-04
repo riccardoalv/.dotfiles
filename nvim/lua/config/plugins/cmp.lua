19f1ac775df0f9dec6d2467e248f49fb39347d29
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-01-04 17:34:03 -0400

    upddate conflitting maps

--- nvim/lua/config/plugins/cmp.lua
+++ nvim/lua/config/plugins/cmp.lua
@@ -1,58 +1,58 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-			"saadparwaiz1/cmp_luasnip",
-			"L3MON4D3/LuaSnip",
-			"rafamadriz/friendly-snippets",
-		},
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+    },
 
-		config = function()
-			local cmp = require("cmp")
+    config = function()
+      local cmp = require("cmp")
 
-			require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-			vim.cmd(
-				[[imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>']]
-			)
-			vim.cmd([[inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>]])
-			vim.cmd([[snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>]])
-			vim.cmd([[snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>]])
+      vim.cmd(
+        [[imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>']]
+      )
+      vim.cmd([[inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>]])
+      vim.cmd([[snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>]])
+      vim.cmd([[snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>]])
 
-			vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
 
-			cmp.setup({
-				experimental = {
-					ghost_text = {
-						hl_group = "CmpGhostText",
-					},
-				},
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				completion = {
-					completeopt = "menu,menuone,preview,noselect",
-				},
-				mapping = {
-					["<C-k>"] = cmp.mapping.select_prev_item(),
-					["<C-j>"] = cmp.mapping.select_next_item(),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({ select = false }),
-					["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-					["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-				},
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "path" },
-					{ name = "buffer" },
-					{ name = "luasnip" },
-				},
-			})
-		end,
-	},
+      cmp.setup({
+        experimental = {
+          ghost_text = {
+            hl_group = "CmpGhostText",
+          },
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
+        mapping = {
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "path" },
+          { name = "buffer" },
+          { name = "luasnip" },
+        },
+      })
+    end,
+  },
 }
--- nvim/lua/config/plugins/navigation.lua
+++ nvim/lua/config/plugins/navigation.lua
@@ -2,56 +2,56 @@ local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 return {
-	"christoomey/vim-tmux-navigator",
+  "christoomey/vim-tmux-navigator",
 
-	{
-		"ThePrimeagen/harpoon",
-		config = function()
-			local status2, harpoon = pcall(require, "harpoon")
-			if not status2 then
-				return
-			end
+  {
+    "ThePrimeagen/harpoon",
+    config = function()
+      local status2, harpoon = pcall(require, "harpoon")
+      if not status2 then
+        return
+      end
 
-			harpoon.setup()
+      harpoon.setup()
 
-			keymap("n", "<leader><leader>", ':lua require("harpoon.mark").add_file()<cr>', opts)
-			keymap("n", "<leader><space>", ':lua require("harpoon.ui").toggle_quick_menu()<cr>', opts)
-			keymap("n", "<c-l>", ':lua require("harpoon.ui").nav_next()<cr>', {})
-			keymap("n", "<c-h>", ':lua require("harpoon.ui").nav_prev()<cr>', {})
-			keymap("n", "<f1>", ':lua require("harpoon.ui").nav_file(1)<cr>', opts)
-			keymap("n", "<f2>", ':lua require("harpoon.ui").nav_file(2)<cr>', opts)
-			keymap("n", "<f3>", ':lua require("harpoon.ui").nav_file(3)<cr>', opts)
-			keymap("n", "<f4>", ':lua require("harpoon.ui").nav_file(4)<cr>', opts)
-			keymap("n", "<f5>", ':lua require("harpoon.ui").nav_file(5)<cr>', opts)
-			keymap("n", "<f6>", ':lua require("harpoon.ui").nav_file(6)<cr>', opts)
-			keymap("n", "<f7>", ':lua require("harpoon.ui").nav_file(7)<cr>', opts)
-			keymap("n", "<f8>", ':lua require("harpoon.ui").nav_file(8)<cr>', opts)
-		end,
-	},
+      keymap("n", "<leader><leader>", ':lua require("harpoon.mark").add_file()<cr>', opts)
+      keymap("n", "<leader><space>", ':lua require("harpoon.ui").toggle_quick_menu()<cr>', opts)
+      keymap("n", "<M-l>", ':lua require("harpoon.ui").nav_next()<cr>', {})
+      keymap("n", "<M-h>", ':lua require("harpoon.ui").nav_prev()<cr>', {})
+      keymap("n", "<f1>", ':lua require("harpoon.ui").nav_file(1)<cr>', opts)
+      keymap("n", "<f2>", ':lua require("harpoon.ui").nav_file(2)<cr>', opts)
+      keymap("n", "<f3>", ':lua require("harpoon.ui").nav_file(3)<cr>', opts)
+      keymap("n", "<f4>", ':lua require("harpoon.ui").nav_file(4)<cr>', opts)
+      keymap("n", "<f5>", ':lua require("harpoon.ui").nav_file(5)<cr>', opts)
+      keymap("n", "<f6>", ':lua require("harpoon.ui").nav_file(6)<cr>', opts)
+      keymap("n", "<f7>", ':lua require("harpoon.ui").nav_file(7)<cr>', opts)
+      keymap("n", "<f8>", ':lua require("harpoon.ui").nav_file(8)<cr>', opts)
+    end,
+  },
 
-	{
-		"phaazon/hop.nvim",
-		config = function()
-			require("hop").setup()
-			keymap("n", "<space>w", ":HopWord<cr>", opts)
-		end,
-	},
+  {
+    "phaazon/hop.nvim",
+    config = function()
+      require("hop").setup()
+      keymap("n", "<space>w", ":HopWord<cr>", opts)
+    end,
+  },
 
-	{
-		"AckslD/nvim-neoclip.lua",
-		config = function()
-			require("neoclip").setup()
-			keymap("n", "<leader>c", [[:Telescope neoclip ]], {})
-			keymap("n", "<A-y>", ':lua require("telescope").extensions.neoclip.default()<cr>', opts)
-		end,
-	},
+  {
+    "AckslD/nvim-neoclip.lua",
+    config = function()
+      require("neoclip").setup()
+      keymap("n", "<leader>c", [[:Telescope neoclip ]], {})
+      keymap("n", "<A-y>", ':lua require("telescope").extensions.neoclip.default()<cr>', opts)
+    end,
+  },
 
-	"ThePrimeagen/git-worktree.nvim",
+  "ThePrimeagen/git-worktree.nvim",
 
-	{
-		"pwntester/octo.nvim",
-		config = function()
-			require("octo").setup()
-		end,
-	},
+  {
+    "pwntester/octo.nvim",
+    config = function()
+      require("octo").setup()
+    end,
+  },
 }
