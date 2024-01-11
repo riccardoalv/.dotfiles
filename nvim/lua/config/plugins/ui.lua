19c2f68b01e40b35ea206ed7fbde4749444ae549
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-01-11 07:10:57 -0400

    remove winblend from fidget

--- nvim/lua/config/plugins/ui.lua
+++ nvim/lua/config/plugins/ui.lua
@@ -39,6 +39,9 @@ return {
       require("fidget").setup({
         notification = {
           override_vim_notify = true,
+          window = {
+            winblend = 0,
+          },
         },
       })
     end,
