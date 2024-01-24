5bde85d5e6aa0554854e9a76459816becdeb9cdd
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-01-24 07:59:22 -0400

    add NIXOS_OZONE_WL variable

--- system/modules/gnome.nix
+++ system/modules/gnome.nix
@@ -8,6 +8,7 @@
   services.gvfs.enable = true;
   services.gnome.sushi.enable = true;
   services.colord.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
   services.xserver = {
     enable = true;
     desktopManager.gnome.enable = true;
