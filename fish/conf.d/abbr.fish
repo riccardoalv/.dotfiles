ef25e0191b6f509402e22d836d1449414469196b
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-01-26 09:44:38 -0400

    update fish alias

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,5 +1,7 @@
 abbr --add dc 'docker-compose'
 abbr --add d 'docker'
-abbr --add clip 'xclip -selection clipboard'
 abbr --add vim 'nvim'
+
+alias zz='cd $(find . -type d -print | fzf)'
+alias clip='xclip -selection clipboard '
 alias vi='nvim -u $HOME/.dotfiles/nvim/cpp.lua '
