7daf919a49f84181a915058a6fe38361b369810d
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-02-04 11:27:53 -0400

    use fish as default shell in alacritty

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,6 +1,7 @@
 abbr --add dc 'docker-compose'
 abbr --add d 'docker'
 abbr --add vim 'nvim'
+abbr --add db 'distrobox'
 
 alias zz='cd $(find . -type d -print | fzf)'
 alias clip='xclip -selection clipboard '
