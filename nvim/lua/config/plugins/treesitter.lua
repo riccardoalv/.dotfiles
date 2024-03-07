e59b296b1dc48a97b2f80ef1120d1f1f531afe88
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-03-07 08:45:50 -0400

    enable autotag rename

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -43,6 +43,9 @@ return {
         },
         autotag = {
           enable = true,
+          enable_rename = true,
+          enable_close = true,
+          enable_close_on_slash = true,
         },
       })
 
