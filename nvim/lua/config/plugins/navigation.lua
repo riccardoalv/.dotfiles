fa4d5c0257346820eab6c9514e77c644d26c8acd
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-03-26 08:22:34 -0400

    update to harpoon2

--- nvim/lua/config/plugins/navigation.lua
+++ nvim/lua/config/plugins/navigation.lua
@@ -6,42 +6,28 @@ return {
 
   {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
     config = function()
       local status2, harpoon = pcall(require, "harpoon")
       if not status2 then
         return
       end
 
-      harpoon.setup()
+      harpoon:setup({
+        settings = {
+          save_on_toggle = true,
+          sync_on_ui_close = true,
+        }
+      })
 
-      keymap("n", "<leader><leader>", require("harpoon.mark").add_file)
-      keymap("n", "<leader><space>", require("harpoon.ui").toggle_quick_menu)
-      keymap("n", "<M-l>", require("harpoon.ui").nav_next)
-      keymap("n", "<M-h>", require("harpoon.ui").nav_prev)
-      keymap("n", "<f1>", function()
-        require("harpoon.ui").nav_file(1)
-      end)
-      keymap("n", "<f2>", function()
-        require("harpoon.ui").nav_file(2)
-      end)
-      keymap("n", "<f3>", function()
-        require("harpoon.ui").nav_file(3)
-      end)
-      keymap("n", "<f4>", function()
-        require("harpoon.ui").nav_file(4)
-      end)
-      keymap("n", "<f5>", function()
-        require("harpoon.ui").nav_file(5)
-      end)
-      keymap("n", "<f6>", function()
-        require("harpoon.ui").nav_file(6)
-      end)
-      keymap("n", "<f7>", function()
-        require("harpoon.ui").nav_file(7)
-      end)
-      keymap("n", "<f8>", function()
-        require("harpoon.ui").nav_file(8)
-      end)
+      keymap("n", "<leader><leader>", function() harpoon:list():append() end)
+      keymap("n", "<leader><space>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      keymap("n", "<M-h>", function() harpoon:list():prev() end)
+      keymap("n", "<M-l>", function() harpoon:list():next() end)
+      keymap("n", "<f1>", function() harpoon:list():select(1) end)
+      keymap("n", "<f2>", function() harpoon:list():select(2) end)
+      keymap("n", "<f3>", function() harpoon:list():select(3) end)
+      keymap("n", "<f4>", function() harpoon:list():select(4) end)
     end,
   },
 
--- system/modules/gnome.nix
+++ system/modules/gnome.nix
@@ -38,6 +38,7 @@
 
   environment.systemPackages = with pkgs; [
     gnome.gnome-tweaks
+    gnome.nautilus-python
     gnome.adwaita-icon-theme
     gnomeExtensions.appindicator
     gnomeExtensions.pop-shell
