752fe02e2f108255c87ca80f8084381760306133
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-04-07 14:21:48 -0400

    remove gitmux

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -94,11 +94,6 @@ return {
             },
             format = {
               enable = false,
-              defaultConfig = {
-                indent_style = "space",
-                indent_size = "2",
-                continuation_indent_size = "2",
-              },
             },
           },
         },
