0a6c6710b9238dd28d242cd441476b74bb3aa4cd
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-04-27 11:02:16 -0400

    sort cmp items

--- nvim/lua/config/plugins/cmp.lua
+++ nvim/lua/config/plugins/cmp.lua
@@ -10,10 +10,25 @@ return {
       "L3MON4D3/LuaSnip",
       "rafamadriz/friendly-snippets",
       "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-emoji",
     },
 
     config = function()
       local cmp = require("cmp")
+      local compare = require("cmp.config.compare")
+      local types = require("cmp.types")
+
+
+      local modified_priority = {
+        [types.lsp.CompletionItemKind.Variable] = types.lsp.CompletionItemKind.Method,
+        [types.lsp.CompletionItemKind.Variable] = types.lsp.CompletionItemKind.Field,
+        [types.lsp.CompletionItemKind.Keyword] = 0,  -- top
+        [types.lsp.CompletionItemKind.Snippet] = 10, -- top
+        [types.lsp.CompletionItemKind.Text] = 100,   -- bottom
+      }
+      local function modified_kind(kind)
+        return modified_priority[kind] or kind
+      end
 
       local has_words_before = function()
         unpack = unpack or table.unpack
@@ -44,6 +59,24 @@ return {
         completion = {
           completeopt = "menu,menuone,preview,noselect",
         },
+        preselect = cmp.PreselectMode.Item,
+        sorting = {
+          comparators = {
+            compare.offset,
+            compare.exact,
+            compare.recently_used,
+            function(entry1, entry2)
+              local kind1 = modified_kind(entry1:get_kind())
+              local kind2 = modified_kind(entry2:get_kind())
+              if kind1 ~= kind2 then
+                return kind1 - kind2 < 0
+              end
+            end,
+            compare.score,
+            compare.order,
+          },
+          priority_weight = 2,
+        },
         mapping = {
           ["<C-k>"] = cmp.mapping.select_prev_item(),
           ["<C-j>"] = cmp.mapping.select_next_item(),
@@ -73,10 +106,11 @@ return {
         },
         sources = {
           { name = "nvim_lsp" },
-          { name = "path" },
           { name = "nvim_lsp_signature_help" },
           { name = "buffer" },
+          { name = "path" },
           { name = 'luasnip' },
+          { name = "emoji" },
         },
       })
     end,
